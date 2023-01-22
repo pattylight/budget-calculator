aefe30d297af1e975582a1cd8c3268452d3f314b
pattylight <kearneypw@gmail.com>
2023-01-22 11:13:51 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import ExpenseList from './components/ExpenseList';
 import ExpenseForm from './components/ExpenseForm';
 import Alert from './components/Alert';
+const { v4: uuid } = require('uuid');
+
+const initialExpenses = [
+  {id:uuid(),charge:"rent",amount:1600},
+  {id:uuid(),charge:"car",amount:2000},
+  {id:uuid(),charge:"food",amount:600}
+];
 
 function App() {
+  //********state values */
+  //all expenses, add expense
+  const [expenses, setExpenses] = useState(initialExpenses);
+  //single expense
+  const [charge, setCharge] = useState('');
+  //single amount
+  const [amount, setAmount] = useState('');
+  
+  //********functions */
+  const handleCharge = e => {
+    setCharge(e.target.value);
+  };
+  const handleAmount = e => {
+    setAmount(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(charge !== '' && amount > 0){
+      const singleExpense = {id:uuid(),charge,amount};
+      setExpenses([...expenses, singleExpense]);
+      setCharge('');
+      setAmount('');
+    }
+    else{
+      //handle alert called
+    }
+  };
+
+
   return <>
+
   <Alert></Alert>
-  <ExpenseForm></ExpenseForm>
-  <ExpenseList></ExpenseList>
+  <h1>Budget Calculator</h1>
+  <main className="App">
+  <ExpenseForm 
+  charge={charge} 
+  amount={amount} 
+  handleAmount={handleAmount}
+  handleCharge={handleCharge} 
+  handleSubmit={handleSubmit}> 
+  </ExpenseForm>
+  <ExpenseList expenses={expenses}></ExpenseList>
+  </main>
+  <h1>
+    total spending : <span className="total"> $ {expenses.reduce((acc, curr)=>{
+      return (acc += parseInt(curr.amount));
+    },0)} </span> 
+  </h1>
+  
   </>;
   
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Item from './ExpenseItem';
+import {MdDelete} from 'react-icons/md';
 
-export const ExpenseList = () => {
+export const ExpenseList = ({expenses}) => {
   return (
-    <div>ExpenseList
-       <Item/> 
-    </div>
+    <>
+        <ul className="list">
+            {expenses.map((expense)=>{
+                return <Item key={expense.id} expense={expense}></Item>;
+            })}
+        </ul>
+        {expenses.length > 0 && <button className='btn'>
+            clear expenses
+            <MdDelete className='btn-icon' />
+            </button>}
+    </>
   )
 }
 export default ExpenseList;
