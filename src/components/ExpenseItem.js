07799541faf96db5019e5a8f5d1d4edc9395e910
pattylight <kearneypw@gmail.com>
2023-01-22 09:54:07 -0500

    Initial  Commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export const ExpenseItem = () => {
+  return (
+    <div>ExpenseItem</div>
+  )
+}
+export default ExpenseItem;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Item from './ExpenseItem';
+
+export const ExpenseList = () => {
+  return (
+    <div>ExpenseList
+       <Item/> 
+    </div>
+  )
+}
+export default ExpenseList;
